b513d0bba39155a7c9a43ad1e312483007f6e0c3
Alois Carrera <aloiscarrera@gmail.com>
2020-10-18 15:47:17 -0500

    feat(user): :sparkles: Add central exporting module
    This is a technique I had never implemented, but is cool beacuse all imports are from same file, it's kind of ordered and clean.

---
+++ src/content/user/user.index.ts
@@ -0,0 +1,3 @@
+export { default as UserApi } from "./routes/user.routes";
+export { default as UserModel } from "./model/user.model";
+export * as UserController from "./controller/user.controller";

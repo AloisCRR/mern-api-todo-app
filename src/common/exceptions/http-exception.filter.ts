c4b3fcd11f2634b641c23517238e2a52c524f067
Alois Carrera <aloiscarrera@gmail.com>
2020-10-24 14:54:59 -0500

    feat(server): :sparkles: Add HttpException class to manage errors
    This class is like entry point to create an error. Goals is to all errors of app pass up here, if not, well maybe I have to research more in error handling.

---
+++ src/common/exceptions/http-exception.filter.ts
@@ -0,0 +1,16 @@
+import { getReasonPhrase, StatusCodes } from "http-status-codes";
+
+export class HttpException extends Error {
+  code: StatusCodes;
+  validationErrors?: object;
+
+  constructor(
+    message: string = getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR),
+    code: StatusCodes = 500,
+    validationErrors?: object
+  ) {
+    super(message);
+    this.code = code;
+    this.validationErrors = validationErrors;
+  }
+}

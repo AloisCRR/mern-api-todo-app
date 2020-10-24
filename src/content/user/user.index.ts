37e5ae9d91233f7c45bdb8a1273758ce772230e9
Alois Carrera <aloiscarrera@gmail.com>
2020-10-24 10:00:17 -0500

    refactor: :fire: Delete older code files
    Most of this files where rewritted in new files, so git shows that as deleted. Anyway this files were a bit inconsistent and very hard to read and mentain.
    
    Refactor code to new project structure

--- src/content/user/user.index.ts
+++
@@ -1,3 +0,0 @@
-export { default as UserApi } from "./routes/user.routes";
-export { default as UserModel } from "./model/user.model";
-export * as UserController from "./controller/user.controller";
